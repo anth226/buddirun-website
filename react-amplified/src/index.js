f975695e36c147723f1738a74fd59f90c3498a5a
Daniel Gagnon <moi@danielgagnon.info>
2022-05-17 22:45:05 -0400

    Fix auth with google

--- react-amplified/src/index.js
+++ react-amplified/src/index.js
@@ -6,8 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import {Amplify} from 'aws-amplify';
 import awsExports from './aws-exports';
 
-Amplify.configure(awsExports);
 awsExports.oauth.domain = process.env.REACT_APP_AUTH_DOMAIN;
+awsExports.oauth.redirectSignIn = `${window.location.origin}/`;
+awsExports.oauth.redirectSignOut = `${window.location.origin}/`;
+Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
